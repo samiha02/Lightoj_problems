74a4e20a191c835bae6522cb71e894afcb5ee50d
Samiha Akram <samiha.angel24@gmail.com>
2016-04-06 23:19:51 +0600

    Added files via upload

---
+++ 1000 - Greetings from LightOJ.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int T,a,b,i,j,sum,ccase=1;
+
+    scanf("%d",&T);
+
+    for(i = 1 ; i <= T ; i++){
+
+        scanf("%d %d",&a,&b);
+
+        sum = a+b;
+
+        printf("Case %d: %d\n",ccase,sum);
+
+        ccase++;
+
+    }
+
+    return 0;
+}
